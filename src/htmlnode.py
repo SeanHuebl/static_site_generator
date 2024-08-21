99067a397879086c82a904182166cfc4fa99b824
Sean Huebl <sean.huebl@gmail.com>
2024-08-20 18:48:19 -0600

    Added to_html method to LeafNode, added unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,8 +24,8 @@ class HTMLNode():
     
 class LeafNode(HTMLNode):
     
-    def __init__(self, value):
-        super().__init__(value=value)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
         self.children = None
         self.value = value
 
@@ -34,5 +34,7 @@ class LeafNode(HTMLNode):
             raise ValueError
         if self.tag == None:
             return self.value
-
-        
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        if self.props:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -18,7 +18,13 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("h1", None, None, {"href": "https://www.google.com", "target": "_blank",})
         print(node.props_to_html())        
-        
+
+    def test_to_html(self):
+        node = LeafNode("p", "hello world")
+        print(node.to_html())
+        node2 = LeafNode("a", "click me!", {"href": "https://www.google.com", "target": "_blank",})
+        print(node2.to_html())
+
          
 
 if __name__ == "__main__":
