93aa90015a3ccccc89c3ff2f73918821405dac6a
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2024-08-27 21:24:32 +0000

    worked on blobk_to_block_type(), created helper functions per type of Block, still implementing full functionality

--- src/enums.py
+++ src/enums.py
@@ -7,3 +7,16 @@ class TextType(Enum):
     CODE = 'code'
     LINK = 'link'
     IMAGE = 'image'
+
+class BlockType(Enum):
+    H1 = '#'
+    H2 = '##'
+    H3 = '###'
+    H4 = '####'
+    H5 = '#####'
+    H6 = '######'
+    CODE = '```'
+    QUOTE = '>'
+    LIST_UNORDERED = ('*', '-')
+    LIST_ORDERED = '. '
+    PARAGRAPH = None
--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,3 +1,7 @@
+import re
+
+from enums import BlockType
+
 def markdown_to_blocks(markdown):
     if not isinstance(markdown, str) or not markdown:
         raise ValueError("Markdown argument must be a non-empty string")
@@ -8,3 +12,32 @@ def markdown_to_blocks(markdown):
             continue
         block_strings.append(block.strip())
     return block_strings
+
+def block_to_block_type(block):
+    heading = check_heading_block(block)
+    if heading:
+        return heading
+    
+            
+        
+            
+def check_heading_block(block):
+    headings = (BlockType.H1, BlockType.H2, BlockType.H3,
+                BlockType.H4, BlockType.H5, BlockType.H6)
+    heading_char = '#'
+    
+    match = re.match(fr'^({heading_char}){{0,6}}', block)
+    
+    if not match.group():
+        return None
+    
+    else:
+        return headings[len(match.group(0)) - 1].value    
+    
+    
+    
+def check_code_block(block):
+    delimiter = '```'
+    sections = block.split(delimiter)
+    print(sections)
+    
