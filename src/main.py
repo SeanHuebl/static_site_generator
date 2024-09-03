317520d1c7abf2374478020fc298be0ba8426932
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2024-09-03 15:32:29 +0000

    Implemented a set of recursive functions to copy all contents from a source to a destination. If destination exists, everything inside the destination dir will be wiped before content is copied in. Otherwise it will create the dest dir before copying all files.

--- src/main.py
+++ src/main.py
@@ -1,27 +1,50 @@
 import os
-
-from enums import TextType
-from textnode import TextNode
+import shutil
 
 def copy_all_contents(source, destination):
-    if not os.path.exists(source) or not os.path.exists(destination):
-        raise ValueError('Check source and destination paths for errors and make sure the paths exist.')
-    return remove_destination_dir_contents(destination)
+    if not os.path.exists(source):
+        raise ValueError('Check source path for errors and make sure the path exists.')
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+        copy_files(source, destination)
+    else:    
+        remove_destination_dir_contents(destination)
+        copy_files(source, destination)
+    print('Copy completed')
+
+def copy_files(source, destination):
+    contents = os.listdir(source)
+    for content in contents:
+        src_path = os.path.join(source, content)
+        dest_path = os.path.join(destination, content)
+        if os.path.isdir(src_path):
+            if not os.path.exists(dest_path):
+                os.mkdir(dest_path)
+            copy_files(src_path, dest_path)
+        elif os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)     
+        
+        else:
+            raise ValueError("Path is not a file nor a directory")
             
-def remove_destination_dir_contents(destination_dir):
-    if os.path.isfile(destination_dir):
-        print(destination_dir)
+def remove_destination_dir_contents(destination_dir):   
+
     contents = os.listdir(destination_dir)
     
     for content in contents:
         path = os.path.join(destination_dir, content)
         if os.path.isdir(path):
             remove_destination_dir_contents(path)
-
+            os.rmdir(path)
+        elif os.path.isfile(path):
+            os.remove(path)
+        else:
+            raise ValueError("Path is not a file nor a directory")
+    
         
 
 def main():
-    print(copy_all_contents('./static', './public'))
+    copy_all_contents('./static', './public')
 
 
 
