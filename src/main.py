0b1845db61b64cfe2720ab11631d39e8cd3e8f8b
Sean Huebl <sean.huebl@gmail.com>
2024-09-03 06:02:44 -0600

    started woring on copy function and starting to write recursive function to clean out the directory that is the destination

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
+import os
+
 from enums import TextType
 from textnode import TextNode
 
+def copy_all_contents(source, destination):
+    if not os.path.exists(source) or not os.path.exists(destination):
+        raise ValueError('Check source and destination paths for errors and make sure the paths exist.')
+    return remove_destination_dir_contents(destination)
+            
+def remove_destination_dir_contents(destination_dir):
+    if os.path.isfile(destination_dir):
+        print(destination_dir)
+    contents = os.listdir(destination_dir)
+    
+    for content in contents:
+        path = os.path.join(destination_dir, content)
+        if os.path.isdir(path):
+            remove_destination_dir_contents(path)
+
+        
 
 def main():
-    first_node = TextNode("Hello", TextType.BOLD, "www.boot.dev")
-    print(first_node)
+    print(copy_all_contents('./static', './public'))
+
 
 
 if __name__ == "__main__":
